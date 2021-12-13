16f364455f2e2d8e06d8e5b087b6b27712db47c6
李心汝 <lixinru@lixinrudeAir.Home>
2021-12-13 20:18:27 +0800

    add to excel

--- cash.py
+++ cash.py
@@ -5,6 +5,7 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格: ') # 如果商品是 q 就不用問 price
+	price = int(price)
 	#p = []
 	#p.append(name)
 	#p.append(price)
@@ -14,3 +15,9 @@ print(products)
 
 for p in products:
 	print(p[0] , '價格是' , p[1])
+
+
+with open('prodcts.csv','w', encoding= 'utf-8') as f:  #寫入和讀取檔案都會遇到編碼問題
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
