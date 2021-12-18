49d28bbd56b5e9793bda7492489e4453d11beaf5
李心汝 <lixinru@lixinrudeAir.Home>
2021-12-18 21:12:07 +0800

    add continue

--- cash.py
+++ cash.py
@@ -1,4 +1,16 @@
+#讀取檔案
 products = []
+products = []
+with open('prodcts.csv', 'r', encoding= 'utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name , price])
+		
+print(products)
+
+
 
 while True:
 	name = input('請輸入商品名稱： ')
